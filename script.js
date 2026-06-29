4027d74e26963f752d4afb775a50a61d35c1151e
sayonar2k <sayonar2k@abcdefg.ru>
2026-06-29 01:31:06 -0700

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const onResize = () => {
+        const width = window.innerWidth;
+
+        if (width < 580) {
+            const buttonMobile = document.querySelector('.mobile-btn');
+            const menuMobile = document.querySelector('.mobile-block');
+            const btnCloseMobileMenu = document.querySelector('.mobile-menu__close');
+
+            buttonMobile.onclick = () => {
+                menuMobile.classList.add('active');
+                document.body.style.overflow = 'hidden';
+            }
+
+            btnCloseMobileMenu.onclick = () => {
+                document.body.style.overflow = '';
+                menuMobile.classList.remove('active');
+            }
+        }
+    }
+
+    onResize();
+
+    window.addEventListener('resize', onResize);
+})
